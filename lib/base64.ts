7683985ca0e767c70bd872887e12923b9fa7c90e
gogogoghost <491511934@qq.com>
2019-09-20 14:32:38 +0800

    base64加入decode

--- lib/base64.ts
+++ lib/base64.ts
@@ -1,9 +1,9 @@
 const _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
 export function encode(input:Uint8Array):string {
-    var output = '';
-    var chr1:i32, chr2:i32, chr3:i32, enc1:i32, enc2:i32, enc3:i32, enc4:i32;
-    var i:i32 = 0;
+    let output = '';
+    let chr1:i32, chr2:i32, chr3:i32, enc1:i32, enc2:i32, enc3:i32, enc4:i32;
+    let i:i32 = 0;
     while (i < input.length) {
         chr1 = input[i++];
 
@@ -22,4 +22,35 @@ export function encode(input:Uint8Array):string {
             _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
     }
     return output;
+}
+
+export function decode(input:string):Uint8Array {
+    //非法字符串
+    if(input.length==0||input.length%4!=0)
+        return new Uint8Array(0);
+
+    let output=new Uint8Array(input.length);
+    let i = 0;
+    let length=0;
+    while(i<input.length){
+        const enc=new Uint8Array(4);
+        for(let j=0;j<4;j++){
+            const c=input.charAt(i++);
+            let index=_keyStr.indexOf(c);
+            if(index<0)
+                return new Uint8Array(0);
+            if(index==64){
+                return output.slice(0,length);
+            }
+            enc[j]=index;
+            if(j==1){
+                output[length++]=(enc[0] << 2) | (enc[1] >> 4)
+            }else if(j==2){
+                output[length++]=((enc[1] & 15) << 4) | (enc[2] >> 2)
+            }else if(j==3){
+                output[length++]=((enc[2] & 3) << 6) | enc[3]
+            }
+        }
+    }
+    return output.slice(0,length);
 }
