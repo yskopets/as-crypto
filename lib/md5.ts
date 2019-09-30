ad94cd9ed279fdcf6f2ffe220e6c577fdf97ce1e
gogogoghost <491511934@qq.com>
2019-09-30 10:33:02 +0800

    代码格式化一下。。。

--- lib/md5.ts
+++ lib/md5.ts
@@ -68,10 +68,10 @@ class Md5 {
     spTouintArray(): Uint32Array {
         const a = new Uint32Array(16);
         for (let i = 0; i < 16; ++i) {
-            let a1:u32 = this.buf[i * 4];
-            let a2:u32 = this.buf[i * 4 + 1];
-            let a3:u32 = this.buf[i * 4 + 2];
-            let a4:u32 = this.buf[i * 4 + 3];
+            let a1: u32 = this.buf[i * 4];
+            let a2: u32 = this.buf[i * 4 + 1];
+            let a3: u32 = this.buf[i * 4 + 2];
+            let a4: u32 = this.buf[i * 4 + 3];
             a[i] = a1 | a2 << 8 | a3 << 16 | a4 << 24;
         }
         return a;
@@ -81,73 +81,73 @@ class Md5 {
         let a = this.virtualVals[0], b = this.virtualVals[1], c = this.virtualVals[2], d = this.virtualVals[3];
         let M = this.spTouintArray();
 
-        a=FF(a, b, c, d, M[0], 7, 0xd76aa478);
-        d=FF(d, a, b, c, M[1], 12, 0xe8c7b756);
-        c=FF(c, d, a, b, M[2], 17, 0x242070db);
-        b=FF(b, c, d, a, M[3], 22, 0xc1bdceee);
-        a=FF(a, b, c, d, M[4], 7, 0xf57c0faf);
-        d=FF(d, a, b, c, M[5], 12, 0x4787c62a);
-        c=FF(c, d, a, b, M[6], 17, 0xa8304613);
-        b=FF(b, c, d, a, M[7], 22, 0xfd469501);
-        a=FF(a, b, c, d, M[8], 7, 0x698098d8);
-        d=FF(d, a, b, c, M[9], 12, 0x8b44f7af);
-        c=FF(c, d, a, b, M[10], 17, 0xffff5bb1);
-        b=FF(b, c, d, a, M[11], 22, 0x895cd7be);
-        a=FF(a, b, c, d, M[12], 7, 0x6b901122);
-        d=FF(d, a, b, c, M[13], 12, 0xfd987193);
-        c=FF(c, d, a, b, M[14], 17, 0xa679438e);
-        b=FF(b, c, d, a, M[15], 22, 0x49b40821);
+        a = FF(a, b, c, d, M[0], 7, 0xd76aa478);
+        d = FF(d, a, b, c, M[1], 12, 0xe8c7b756);
+        c = FF(c, d, a, b, M[2], 17, 0x242070db);
+        b = FF(b, c, d, a, M[3], 22, 0xc1bdceee);
+        a = FF(a, b, c, d, M[4], 7, 0xf57c0faf);
+        d = FF(d, a, b, c, M[5], 12, 0x4787c62a);
+        c = FF(c, d, a, b, M[6], 17, 0xa8304613);
+        b = FF(b, c, d, a, M[7], 22, 0xfd469501);
+        a = FF(a, b, c, d, M[8], 7, 0x698098d8);
+        d = FF(d, a, b, c, M[9], 12, 0x8b44f7af);
+        c = FF(c, d, a, b, M[10], 17, 0xffff5bb1);
+        b = FF(b, c, d, a, M[11], 22, 0x895cd7be);
+        a = FF(a, b, c, d, M[12], 7, 0x6b901122);
+        d = FF(d, a, b, c, M[13], 12, 0xfd987193);
+        c = FF(c, d, a, b, M[14], 17, 0xa679438e);
+        b = FF(b, c, d, a, M[15], 22, 0x49b40821);
 
-        a=GG(a, b, c, d, M[1], 5, 0xf61e2562);
-        d=GG(d, a, b, c, M[6], 9, 0xc040b340);
-        c=GG(c, d, a, b, M[11], 14, 0x265e5a51);
-        b=GG(b, c, d, a, M[0], 20, 0xe9b6c7aa);
-        a=GG(a, b, c, d, M[5], 5, 0xd62f105d);
-        d=GG(d, a, b, c, M[10], 9, 0x2441453);
-        c=GG(c, d, a, b, M[15], 14, 0xd8a1e681);
-        b=GG(b, c, d, a, M[4], 20, 0xe7d3fbc8);
-        a=GG(a, b, c, d, M[9], 5, 0x21e1cde6);
-        d=GG(d, a, b, c, M[14], 9, 0xc33707d6);
-        c=GG(c, d, a, b, M[3], 14, 0xf4d50d87);
-        b=GG(b, c, d, a, M[8], 20, 0x455a14ed);
-        a=GG(a, b, c, d, M[13], 5, 0xa9e3e905);
-        d=GG(d, a, b, c, M[2], 9, 0xfcefa3f8);
-        c=GG(c, d, a, b, M[7], 14, 0x676f02d9);
-        b=GG(b, c, d, a, M[12], 20, 0x8d2a4c8a);
+        a = GG(a, b, c, d, M[1], 5, 0xf61e2562);
+        d = GG(d, a, b, c, M[6], 9, 0xc040b340);
+        c = GG(c, d, a, b, M[11], 14, 0x265e5a51);
+        b = GG(b, c, d, a, M[0], 20, 0xe9b6c7aa);
+        a = GG(a, b, c, d, M[5], 5, 0xd62f105d);
+        d = GG(d, a, b, c, M[10], 9, 0x2441453);
+        c = GG(c, d, a, b, M[15], 14, 0xd8a1e681);
+        b = GG(b, c, d, a, M[4], 20, 0xe7d3fbc8);
+        a = GG(a, b, c, d, M[9], 5, 0x21e1cde6);
+        d = GG(d, a, b, c, M[14], 9, 0xc33707d6);
+        c = GG(c, d, a, b, M[3], 14, 0xf4d50d87);
+        b = GG(b, c, d, a, M[8], 20, 0x455a14ed);
+        a = GG(a, b, c, d, M[13], 5, 0xa9e3e905);
+        d = GG(d, a, b, c, M[2], 9, 0xfcefa3f8);
+        c = GG(c, d, a, b, M[7], 14, 0x676f02d9);
+        b = GG(b, c, d, a, M[12], 20, 0x8d2a4c8a);
 
-        a=HH(a, b, c, d, M[5], 4, 0xfffa3942);
-        d=HH(d, a, b, c, M[8], 11, 0x8771f681);
-        c=HH(c, d, a, b, M[11], 16, 0x6d9d6122);
-        b=HH(b, c, d, a, M[14], 23, 0xfde5380c);
-        a=HH(a, b, c, d, M[1], 4, 0xa4beea44);
-        d=HH(d, a, b, c, M[4], 11, 0x4bdecfa9);
-        c=HH(c, d, a, b, M[7], 16, 0xf6bb4b60);
-        b=HH(b, c, d, a, M[10], 23, 0xbebfbc70);
-        a=HH(a, b, c, d, M[13], 4, 0x289b7ec6);
-        d=HH(d, a, b, c, M[0], 11, 0xeaa127fa);
-        c=HH(c, d, a, b, M[3], 16, 0xd4ef3085);
-        b=HH(b, c, d, a, M[6], 23, 0x4881d05);
-        a=HH(a, b, c, d, M[9], 4, 0xd9d4d039);
-        d=HH(d, a, b, c, M[12], 11, 0xe6db99e5);
-        c=HH(c, d, a, b, M[15], 16, 0x1fa27cf8);
-        b=HH(b, c, d, a, M[2], 23, 0xc4ac5665);
+        a = HH(a, b, c, d, M[5], 4, 0xfffa3942);
+        d = HH(d, a, b, c, M[8], 11, 0x8771f681);
+        c = HH(c, d, a, b, M[11], 16, 0x6d9d6122);
+        b = HH(b, c, d, a, M[14], 23, 0xfde5380c);
+        a = HH(a, b, c, d, M[1], 4, 0xa4beea44);
+        d = HH(d, a, b, c, M[4], 11, 0x4bdecfa9);
+        c = HH(c, d, a, b, M[7], 16, 0xf6bb4b60);
+        b = HH(b, c, d, a, M[10], 23, 0xbebfbc70);
+        a = HH(a, b, c, d, M[13], 4, 0x289b7ec6);
+        d = HH(d, a, b, c, M[0], 11, 0xeaa127fa);
+        c = HH(c, d, a, b, M[3], 16, 0xd4ef3085);
+        b = HH(b, c, d, a, M[6], 23, 0x4881d05);
+        a = HH(a, b, c, d, M[9], 4, 0xd9d4d039);
+        d = HH(d, a, b, c, M[12], 11, 0xe6db99e5);
+        c = HH(c, d, a, b, M[15], 16, 0x1fa27cf8);
+        b = HH(b, c, d, a, M[2], 23, 0xc4ac5665);
 
-        a=II(a, b, c, d, M[0], 6, 0xf4292244);
-        d=II(d, a, b, c, M[7], 10, 0x432aff97);
-        c=II(c, d, a, b, M[14], 15, 0xab9423a7);
-        b=II(b, c, d, a, M[5], 21, 0xfc93a039);
-        a=II(a, b, c, d, M[12], 6, 0x655b59c3);
-        d=II(d, a, b, c, M[3], 10, 0x8f0ccc92);
-        c=II(c, d, a, b, M[10], 15, 0xffeff47d);
-        b=II(b, c, d, a, M[1], 21, 0x85845dd1);
-        a=II(a, b, c, d, M[8], 6, 0x6fa87e4f);
-        d=II(d, a, b, c, M[15], 10, 0xfe2ce6e0);
-        c=II(c, d, a, b, M[6], 15, 0xa3014314);
-        b=II(b, c, d, a, M[13], 21, 0x4e0811a1);
-        a=II(a, b, c, d, M[4], 6, 0xf7537e82);
+        a = II(a, b, c, d, M[0], 6, 0xf4292244);
+        d = II(d, a, b, c, M[7], 10, 0x432aff97);
+        c = II(c, d, a, b, M[14], 15, 0xab9423a7);
+        b = II(b, c, d, a, M[5], 21, 0xfc93a039);
+        a = II(a, b, c, d, M[12], 6, 0x655b59c3);
+        d = II(d, a, b, c, M[3], 10, 0x8f0ccc92);
+        c = II(c, d, a, b, M[10], 15, 0xffeff47d);
+        b = II(b, c, d, a, M[1], 21, 0x85845dd1);
+        a = II(a, b, c, d, M[8], 6, 0x6fa87e4f);
+        d = II(d, a, b, c, M[15], 10, 0xfe2ce6e0);
+        c = II(c, d, a, b, M[6], 15, 0xa3014314);
+        b = II(b, c, d, a, M[13], 21, 0x4e0811a1);
+        a = II(a, b, c, d, M[4], 6, 0xf7537e82);
         d = II(d, a, b, c, M[11], 10, 0xbd3af235);
-        c=II(c, d, a, b, M[2], 15, 0x2ad7d2bb);
-        b=II(b, c, d, a, M[9], 21, 0xeb86d391);
+        c = II(c, d, a, b, M[2], 15, 0x2ad7d2bb);
+        b = II(b, c, d, a, M[9], 21, 0xeb86d391);
 
         this.virtualVals[0] = a + this.virtualVals[0];
         this.virtualVals[1] = b + this.virtualVals[1];
@@ -214,6 +214,7 @@ class Md5 {
     }
 
 }
+
 const hexCharList: string[] = '0123456789abcdef'.split('');
 
 //计算32位md5
@@ -229,11 +230,13 @@ function getHexStr(arr: Uint8Array): string {
 export function hash(data: Uint8Array): Uint8Array {
     return (new Md5(data)).getResult();
 }
+
 //获取32位
-export function hex32(data:Uint8Array):string {
+export function hex32(data: Uint8Array): string {
     return getHexStr((new Md5(data)).getResult());
 }
+
 //获取16位
-export function hex16(data:Uint8Array):string {
-    return hex32(data).substring(8,24);
+export function hex16(data: Uint8Array): string {
+    return hex32(data).substring(8, 24);
 }
